f4d69fe148a9d1a00021a69042313440e87d1549
wudeshi1991 <wudeshi19910415@gmail.com>
2015-12-15 07:33:22 -0500

    quick version

--- CpuScheduler.h
+++ CpuScheduler.h
@@ -1,14 +1,35 @@
 #ifndef CPUSCHEDULER_H_INCLUDED
 #define CPUSCHEDULER_H_INCLUDED
-using namespace std;
+#include <iostream>
+#include <vector>
 
-//This class will return job number
-//use any algoruthm to schedule the next job from memory - Deshi Wu
-class CpuScheduler {
-private:
+using namespace std;
+bool cpuOnTrace=false;
+
+
+// This Class is use to schedule next job from memory
+// Using Shortest Job first Algorithm
+// Find shortest job in Memory ---Deshi Wu
+class cpuScheduler {
 
 public:
 
-};
+    long shortestJob(vector<Job> jobsList) {
 
-#endif // CPUSCHEDULER_H_INCLUDED
+        // default set to -1 (indicates job is not found)
+        long shortJobNum = -1, i, minTime=1000000;
+
+        for(i=0; i<jobsList.size(); i++) {
+            // first checks that conditions for proper dispatching apply
+            if(jobsList[i].isInMemory() && jobsList[i].getMaxCpuTime()!=0 && !jobsList[i].isTerminated() && !jobsList[i].isBlocked()) {
+                // then looks for the job with the shortest remaining time left.
+                if(jobsList[i].getMaxCpuTime() < minTime ) {
+                    minTime = jobsList[i].getMaxCpuTime();
+                    shortJobNum = i;
+             }
+            }
+           }
+        return shortJobNum;
+    }
+};
+#endif// CPUSCHEDULER_H_INCLUDED
